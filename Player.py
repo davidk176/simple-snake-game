40dc9c2b8025a0332a37107c452757b703b7bb80
davidk176 <david.karenke@live.com>
2020-04-13 13:40:18 +0200

    added some documentation

--- Player.py
+++ Player.py
@@ -63,21 +63,26 @@ class Food:
         
   
 class Game:
+    #Init Frame Size
     frame_size_x=1048
     frame_size_y=720
-    snake = Snake()
+    
+    #Colors
     green = pygame.Color(0,255,0)
     black = pygame.Color(0,0,0)
     red = pygame.Color(255,0,0)
+    
+    #Game objects
     food = Food()
+    snake = Snake()
     
     def get_Frame_Size(self):
-        print("")
+        return [frame_size_x, frame_size_y]
         
-    #Do init      
+    #Start Game logic with init     
     check_errors = pygame.init()
     if check_errors[1] > 0:
-        print(f'[!] Had {check_errors[1]} errors when initialising game, exiting...')
+        print(f'[!] Error while initialising game')
         sys.exit(-1)
     else:
         print('[+] Game successfully initialised')
@@ -91,9 +96,9 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        # Whenever a key is pressed down
+        # on key is pressed down
             if event.type == pygame.KEYDOWN:
-            # W -> Up; S -> Down; A -> Left; D -> Right
+            #  WASD or arrow keys
                 if event.key == pygame.K_UP or event.key == ord('w'):
                     snake.change_to = 'UP'
                 if event.key == pygame.K_DOWN or event.key == ord('s'):
@@ -102,12 +107,14 @@ class Game:
                     snake.change_to = 'LEFT'
                 if event.key == pygame.K_RIGHT or event.key == ord('d'):
                     snake.change_to = 'RIGHT'
-                # Esc -> Create event to quit the game
+                # Esc -> exit
                 if event.key == pygame.K_ESCAPE:
                     pygame.event.post(pygame.event.Event(pygame.QUIT))
-                    
+        #change direction of snake
         snake.change_direction()
+        #move snake
         snake.move()
+        #spawn food
         food.spawn(frame_size_x, frame_size_y, snake)
         game_window.fill(black)
         #Draw Snake
